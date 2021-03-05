ce474aadd5b818f7cdf0e82934a411f3d6644b3d
Nicoll <rosamili45@gmail.com>
2021-03-04 23:36:11 -0500

    Correcion

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/','LoginController@index'); 
+Route::get('/',function()
+{
+	return view('inicio');
+});
 
+/*Route::get('/inicio','LoginController@index');*/
