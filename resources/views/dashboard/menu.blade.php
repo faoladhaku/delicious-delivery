33c9db1d8c12b73ce7847b7e11943322fdaa0ac1
Basurco <angelsoulsword@gmail.com>
2021-03-15 17:18:37 -0500

    kill me pls

--- resources/views/dashboard/menu.blade.php
+++ resources/views/dashboard/menu.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 Porfavor Seleccione la comida que desea para el dia Lunes
 <select name="comidas" id="comidas" class="form-control">
-    @foreach($item as $item)
-    <option value="{{ $item->id }}">{{ $item->name }}</option>
-    @endforeach
 </select>
+
+{{$items}}
+
 @endsection
