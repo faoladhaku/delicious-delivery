6e75db314fa63994d6991e13d44857a4abfab699
Diego[GTZ] <dartz160@gmail.com>
2021-03-16 10:30:18 -0500

    nuevos sedders

--- database/seeders/ComidasSeeder.php
+++ database/seeders/ComidasSeeder.php
@@ -20,6 +20,28 @@ class ComidasSeeder extends Seeder
             'url'=> '\\\\',
 
         ]);
+        DB::table('comidas')->insert([
+
+            'nombre'=> 'Locro',
+            'descripcion'=>'El locro (del quechua ruqru o luqru) ​ es una especie de guiso de origen prehispánico y preincaico, típico de varios pueblos andinos, a base de zapallo',
+            'url'=> '\\\\',
+
+        ]);
+        DB::table('comidas')->insert([
+
+            'nombre'=> 'Ají de gallina',
+            'descripcion'=>'
+            El ají de gallina es un plato típico de la gastronomía peruana, concretamente de la costa',
+            'url'=> '\\\\',
+
+        ]);
+        DB::table('comidas')->insert([
+
+            'nombre'=> 'Arroz chaufa',
+            'descripcion'=>'El arroz chaufa se ha convertido en uno de los platos más solicitados en el Perú. Sus orígenes se remontan al siglo XIX, donde surge a partir de una fusión de culturas',
+            'url'=> '\\\\',
+
+        ]);
         DB::table('bebidas')->insert([
 
             'nombre'=> 'Coca-Cola',
@@ -27,10 +49,52 @@ class ComidasSeeder extends Seeder
             'url'=> '\\\\',
 
         ]);
+        DB::table('bebidas')->insert([
+
+            'nombre'=> 'Inca Kola',
+            'descripcion'=>'Inca Kola es una bebida gaseosa peruana. Tiene un sabor dulce y un color amarillo-dorado. ',
+            'url'=> '\\\\',
+
+        ]);
+        DB::table('bebidas')->insert([
+
+            'nombre'=> 'Kola Escocesa',
+            'descripcion'=>'Kola Escocesa es un refresco peruano, marca de la compañía Yura, ubicada en la ciudad de Yura',
+            'url'=> '\\\\',
+
+        ]);
+        DB::table('bebidas')->insert([
+
+            'nombre'=> 'Fanta',
+            'descripcion'=>'Fanta es una marca de refresco con gas, propiedad de la compañía Coca-Cola',
+            'url'=> '\\\\',
+
+        ]);
         DB::table('postres')->insert([
 
             'nombre'=> 'mazamorra',
             'descripcion'=>'La mazamorra más conocida y consumida en el Perú es la limeña mazamorra morada',
+            'url'=> '\\\\',
+
+        ]);
+        DB::table('postres')->insert([
+
+            'nombre'=> 'Gelatina',
+            'descripcion'=>'',
+            'url'=> '\\\\',
+
+        ]);
+        DB::table('postres')->insert([
+
+            'nombre'=> 'Gelatina - Fresa',
+            'descripcion'=>'',
+            'url'=> '\\\\',
+
+        ]);
+        DB::table('postres')->insert([
+
+            'nombre'=> 'Helado de Fresa',
+            'descripcion'=>'',
             'url'=> '\\\\',
 
         ]);
--- resources/views/Pedidos.blade.php
+++ resources/views/Pedidos.blade.php
@@ -1,26 +1,30 @@
 @extends('layout')
 @section('content')
-<h1>Pedidos Realizados </h1>
 
-<h2>Usuario : {{$name->name}}</h2>
+<h1 class="text-center"> Pedidos Realizados</h1>
 
-<table class="">
-    <tr>
-        <th>Fecha de Inicio</th>
-        <th>Fecha Final</th>
-        <th>Costo</th>
-        <th></th>
-    </tr>
-@foreach ($items as $item)
-    <tr>
-        <td>{{$item->Fecha_Inicio}}</td>
-        <td>{{$item->Fecha_Final}}</td>
-        <td>{{$item->costo}}</td>
-        <td><a class="btn btn-outline-secondary" href="#" >Ver</a></td>
-    </tr>
-
-    
-    
-@endforeach
+<div class="container">
+    <div class="row">
+        <div class = " col">
+            <table class="table table-sm" style="margin: 0 auto;">
+                <tr>
+                    <th>#</th>
+                    <th>Fecha de Inicio</th>
+                    <th>Fecha Final</th>
+                    <th>Costo</th>
+                    <th></th>
+                </tr>
+            @foreach ($items as $item)
+                <tr>
+                    <td> </td>
+                    <td>{{$item->Fecha_Inicio}}</td>
+                    <td>{{$item->Fecha_Final}}</td>
+                    <td>{{$item->costo}}</td>
+                    <td><a class="btn btn-outline-secondary" href="{{ route('Pedido.id', [$item->id_pedidos_comida]) }}" >Ver</a></td>
+                </tr>
+            @endforeach       
+        </div>
+    </div>
+</div>
 
 @endsection
